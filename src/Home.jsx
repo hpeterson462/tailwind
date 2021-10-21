de9948915e7f215ed8f090cbc98ba3877a898611
Heather Peterson <hpeterson462@gmail.com>
2021-10-21 10:40:51 -0700

    h1 title centered, responsive

--- src/Home.jsx
+++ src/Home.jsx
@@ -2,8 +2,10 @@ import React from 'react';
 
 export default function Home() {
   return (
-    <>
-      Home
-    </>
+    <main className='fixed overflow-hidden text-center left-0 right-0'>
+      <h1 className='text-7xl mt-16 font-header lg:text-8xl'>
+        Heather Peterson
+      </h1>
+    </main>
   );
 }
