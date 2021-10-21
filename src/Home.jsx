3b50c771d65a85a4c645dc8c5c20780d58e357ce
Heather Peterson <hpeterson462@gmail.com>
2021-10-21 14:31:25 -0700

    subtitle bg color for contrast

--- src/Home.jsx
+++ src/Home.jsx
@@ -3,10 +3,10 @@ import React from 'react';
 export default function Home() {
   return (
     <main className='fixed overflow-hidden text-center left-0 right-0'>
-      <h1 className='mt-16 font-header lg:text-8xl'>
+      <h1 className='mt-16 font-header'>
         Heather Peterson
       </h1>
-      <h3 className='text-lightBlue font-body'>
+      <h3 className='mt-2 text-lightBlue font-body bg-darkGray lg:mx-80 lg:rounded-lg'>
         Frontend Software Developer
       </h3>
       <div className='bg-dots bg-center bg-no-repeat min-h-screen'></div>
