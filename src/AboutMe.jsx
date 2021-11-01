2a8d40a534f51d559648723428be1f9f5ddf5577
Heather Peterson <hpeterson462@gmail.com>
2021-11-01 13:10:16 -0700

    responsive p centering

--- src/AboutMe.jsx
+++ src/AboutMe.jsx
@@ -12,13 +12,13 @@ export default function AboutMe() {
 
       <br />
 
-      <main className='flex flex-row flex-wrap italic font-body justify-evenly items-center bg-lightGray min-h-screen'>
+      <main className='flex flex-wrap italic font-body justify-evenly items-center bg-lightGray md:pb-4'>
         <img
-          className='mt-5 m-auto h-1/2 w-80 rounded-lg'
+          className='mt-5 m-auto w-80 rounded-lg'
           src={heather}
           alt='Heather'
         />
-        <p className='m-5 text-left font-body leading-10'>
+        <p className='m-5 max-w-xl text-left font-body leading-10'>
           I’m a frontend software developer with a passion for user experiences, solving human-based pain points, design, collaborating on diverse teams, and creative problem-solving. I rely on test-driven development to create JavaScript and React user interfaces with CSS and Sass designs. I consistently bring positive energy, a hard work ethic, and an endless curiosity for knowledge! I’m excited to continue to discover, learn, and grow with amazing people.
         </p>
       </main>
